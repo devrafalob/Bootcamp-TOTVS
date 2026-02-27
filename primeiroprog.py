7ae98fe3a097f6d4cbacfcb6e5005f5e398b44a4
Rafael <rafael.lobao104@academico.ifs.edu.br>
2026-02-26 21:03:39 -0300

    Arit

--- primeiroprog.py
+++ primeiroprog.py
@@ -1,1 +1,14 @@
-float("a")
+#soma
+print(1+2)
+#subtração
+print(2-1)
+#divisão 
+print(3/3)
+#divisao inteira
+print(5//2)
+#multiplicação
+print(2*2)
+#exponenciacao
+print(3**2)
+#modulo/sobra de uma divisao
+print(20%3)
