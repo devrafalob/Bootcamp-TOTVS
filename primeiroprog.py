6bc286c7b42565b175e7fcf9fa93a892d0b67668
Rafael <rafael.lobao104@academico.ifs.edu.br>
2026-03-02 23:02:38 -0300

    Praticando

--- primeiroprog.py
+++ primeiroprog.py
@@ -3,45 +3,44 @@ i = 0
 aprovados = 0
 reprovados = 0
 media_turma = 0
-aluno_maior_media = ""
-aluno_menor_media = ""
 
-while quantidade_alunos != i:
-    i+=1
-    notas = []
-    maior_media = 0
-    menor_media = 10
+def calcular_media(n1, n2, n3) -> int:
+    media = (n1+n2+n3)/3
+    return media
 
-    nome = input("Nome: ")
-    for a in range(1,4):
-        notas.append(int(input("Notas: ")))
-
-    media = (notas[0] + notas[1] + notas[2])/3
-    
-    media_turma += media
-    
-    print("Média do aluno {}".format(media))
+def situacao(media:int):
+    global aprovados
+    global reprovados
     if media >= 7:
         print("Aprovado")
         aprovados += 1
-    elif 5<= media < 7:
+    elif 5 <= media < 7:
         print("Prova Final")
     else:
         print("Reprovado")
         reprovados += 1
-    
-    if quantidade_alunos == 1:
-        aluno_menor_media = aluno_maior_media = nome
-    elif media < menor_media:
-        aluno_menor_media = nome
-    else:
-        aluno_maior_media = nome
-        
 
-media_turma/=quantidade_alunos
+def calcular_media_turma(media):
+    global quantidade_alunos
+    global media_turma
+    media_turma += media
+    media_turma /= quantidade_alunos
+    return media_turma
+
+while quantidade_alunos != i:
+    i+=1
+    notas = []
+
+    nome = input("Nome: ")
+    nota_1 = int(input("Notas: "))
+    nota_2 = int(input("Notas: "))
+    nota_3 = int(input("Notas: "))
+
+    calcular_media_turma(calcular_media(nota_1, nota_2, nota_3))
+    situacao(calcular_media(nota_1, nota_2, nota_3))
+    
+
 
 print("Numero de aprovados {}".format(aprovados))
 print("Numero de reprovados {}".format(reprovados))
 print("Média da turma {}".format(media_turma))
-print("Aluno com maior media {}".format(aluno_maior_media))
-print("Aluno com menor media {}".format(aluno_menor_media))
