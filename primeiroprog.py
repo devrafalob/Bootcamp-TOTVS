29ff68dd6ddd97912c499bddb94bf2ddec04ddfa
Rafael <rafael.lobao104@academico.ifs.edu.br>
2026-03-03 22:33:40 -0300

    ranking

--- primeiroprog.py
+++ primeiroprog.py
@@ -3,10 +3,15 @@ i = 0
 aprovados = 0
 reprovados = 0
 media_turma = 0
+maior_media = None
+menor_media = None
+aluno_maior_media = ""
+aluno_menor_media = ""
 
-def calcular_media(n1, n2, n3) -> int:
+def calcular_media(n1, n2, n3, nome_aluno) -> int:
     global media_turma
     media = (n1+n2+n3)/3
+    ranking_media(media, nome_aluno)
     media_turma += media
     return media
 
@@ -27,6 +32,24 @@ def calcular_media_turma():
     global media_turma
     media_turma /= quantidade_alunos
 
+def ranking_media(media, nome_aluno):
+    global aluno_maior_media
+    global aluno_menor_media
+    global maior_media
+    global menor_media
+    if maior_media is None:
+        aluno_maior_media = nome_aluno
+        aluno_menor_media = nome_aluno
+        maior_media = media
+        menor_media = media
+    elif media > maior_media:
+        aluno_maior_media = nome_aluno
+        maior_media = media
+    elif media < menor_media:
+        aluno_menor_media= nome_aluno
+        menor_media = media
+
+
 while quantidade_alunos != i:
     i+=1
     notas = []
@@ -36,7 +59,7 @@ while quantidade_alunos != i:
     nota_2 = int(input("Notas: "))
     nota_3 = int(input("Notas: "))
 
-    situacao(calcular_media(nota_1, nota_2, nota_3))
+    situacao(calcular_media(nota_1, nota_2, nota_3, nome))
 
     if i == quantidade_alunos:
         calcular_media_turma()
@@ -44,3 +67,5 @@ while quantidade_alunos != i:
 print("Numero de aprovados {}".format(aprovados))
 print("Numero de reprovados {}".format(reprovados))
 print("Média da turma {}".format(media_turma))
+print("Aluno maior nota: {}".format(aluno_maior_media))
+print("Aluno menor Nota: {}".format(aluno_menor_media))
